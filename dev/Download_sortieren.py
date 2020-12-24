80e5ae9ad4de050dddf39725935b8cbcc70f8aa9
Findus2020 <m.karmanski@web.de>
2020-12-24 12:02:28 +0100

    Ausführende Funktion in main verschoben

--- dev/Download_sortieren.py
+++ dev/Download_sortieren.py
@@ -2,24 +2,6 @@
 import os
 import shutil
 
-if __name__ == '__main__':
-    cleanOrdner = "C:/Users/mkarm/Downloads/"
-    ziel_beispiel = "C:/Users/mkarm/OneDrive/Michi/Versicherungen/"
-    dateien = os.listdir(cleanOrdner)
-    print(dateien)
-    print(dateien[0])
-
-    for i in dateien:
-        print(f"i: {i}")
-        if 'Beispiel' in i:
-            print("Gefunden - Element Found in Tuple")
-            pfad = cleanOrdner + i
-            print(pfad)
-            shutil.move(pfad, ziel_beispiel)
-        #    else:
-        # print("Element not Found in Tuple")
-
-
 def clean_folder(name, subject, folder, target):
     folder_list = os.listdir(folder)
     k = 1
@@ -34,15 +16,6 @@ def clean_folder(name, subject, folder, target):
                 f"Datei: {i}, Quelle: {folder}, Ziel: {target}")
 
     print(f"{name}: Aufräumen beendet")
-
-
-# Audi-Bank
-clean_folder("Audi-Bank", "Kontoauszug-1519125726", "C:/Users/mkarm/Downloads/",
-             "C:/Users/mkarm/OneDrive/Michi/Konto/Audi Bank/Kontoauszug 2020")
-# Renault-Bank
-clean_folder("Renault-Bank", "29131500", "C:/Users/mkarm/Downloads/",
-             "C:/Users/mkarm/OneDrive/Michi/Konto/Renault Bank/Kontoauszug/2020")
-
 
 
 #exe bauen und in autostart hinzufügen
--- dev/main.py
+++ dev/main.py
@@ -3,4 +3,9 @@ from Download_sortieren import clean_folder
 # if __name__ == '__main__':
 #    clean_folder(subject, folder, target)
 
-print("test main")
+# Audi-Bank
+clean_folder("Audi-Bank", "Kontoauszug-1519125726", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Konto/Audi Bank/Kontoauszug 2020")
+# Renault-Bank
+clean_folder("Renault-Bank", "29131500", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Konto/Renault Bank/Kontoauszug/2020")
