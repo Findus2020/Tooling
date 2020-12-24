7c776be1f720ec8a7087295ddfb5adc02540bd23
Findus2020 <m.karmanski@web.de>
2020-12-24 12:00:42 +0100

    Reformat Code

--- dev/Download_sortieren.py
+++ dev/Download_sortieren.py
@@ -17,30 +17,33 @@ if __name__ == '__main__':
             print(pfad)
             shutil.move(pfad, ziel_beispiel)
         #    else:
-            # print("Element not Found in Tuple")
+        # print("Element not Found in Tuple")
 
-def clean_folder(subject,folder,target):
+
+def clean_folder(name, subject, folder, target):
     folder_list = os.listdir(folder)
     k = 1
     for i in folder_list:
-        #print(f"{k}. Datei: {i}") #Ausgabe aller Datein im Quellordner
+        # print(f"{k}. Datei: {i}") #Ausgabe aller Datein im Quellordner
         k += 1
         if subject in i:
             print(f"{i} gefunden")
             path = folder + i
             shutil.move(path, target)
             print(
-            f"Datei: {i}, Quelle: {folder}, Ziel: {target}")
+                f"Datei: {i}, Quelle: {folder}, Ziel: {target}")
 
-#Audi-Bank
-clean_folder("Kontoauszug-1519125726", "C:/Users/mkarm/Downloads/", "C:/Users/mkarm/OneDrive/Michi/Konto/Audi Bank/Kontoauszug 2020")
-#Renault-Bank
-clean_folder("29131500", "C:/Users/mkarm/Downloads/", "C:/Users/mkarm/OneDrive/Michi/Konto/Renault Bank/Kontoauszug/2020")
+    print(f"{name}: Aufräumen beendet")
 
 
+# Audi-Bank
+clean_folder("Audi-Bank", "Kontoauszug-1519125726", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Konto/Audi Bank/Kontoauszug 2020")
+# Renault-Bank
+clean_folder("Renault-Bank", "29131500", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Konto/Renault Bank/Kontoauszug/2020")
 
-#funktion schreiben mit Input: gesuchterDateiname, aufzuräumenderOrdner, Zielort
-#wenn mehere zutreffende Datein im Verzeichnis, dann alle verschieben mit jeweils einer ausgabe
-#Ausgabe dass erfolgreich verschoben wurde
+
+
 #exe bauen und in autostart hinzufügen
 #Buch S. 325
--- dev/main.py
+++ dev/main.py
@@ -1,4 +1,3 @@
-#import Download_sortieren
 from Download_sortieren import clean_folder
 
 # if __name__ == '__main__':
