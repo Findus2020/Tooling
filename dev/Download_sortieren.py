c69d775f946476b75e2ae723010b816e752cf08d
Findus2020 <m.karmanski@web.de>
2020-12-24 11:43:11 +0100

    Erste, funktionierende Funktion clean_Folder

--- dev/Download_sortieren.py
+++ dev/Download_sortieren.py
@@ -2,25 +2,41 @@
 import os
 import shutil
 
-cleanOrdner = "C:/Users/mkarm/Downloads/"
-ziel_beispiel = "C:/Users/mkarm/OneDrive/Michi/Versicherungen/"
-dateien = os.listdir(cleanOrdner)
-print(dateien)
-print(dateien[0])
+if __name__ == '__main__':
+    cleanOrdner = "C:/Users/mkarm/Downloads/"
+    ziel_beispiel = "C:/Users/mkarm/OneDrive/Michi/Versicherungen/"
+    dateien = os.listdir(cleanOrdner)
+    print(dateien)
+    print(dateien[0])
 
-for i in dateien:
-    print(f"i: {i}")
-    if 'Beispiel' in i:
-        print("Gefunden - Element Found in Tuple")
-        pfad = cleanOrdner + i
-        print(pfad)
-        shutil.move(pfad, ziel_beispiel)
-#    else:
-# print("Element not Found in Tuple")
+    for i in dateien:
+        print(f"i: {i}")
+        if 'Beispiel' in i:
+            print("Gefunden - Element Found in Tuple")
+            pfad = cleanOrdner + i
+            print(pfad)
+            shutil.move(pfad, ziel_beispiel)
+        #    else:
+            # print("Element not Found in Tuple")
 
-print("Aufräumen beendet")
+def clean_folder(subject,folder,target):
+    print(f"Die Datei zum Thema {subject} aus dem Ordner {folder} wird in das Zielverzeichnis {target} verschoben")
+    folder_list = os.listdir(folder)
+    k = 1
+    for i in folder_list:
+        print(f"{k}. Datei: {i}")
+        k += 1
+        if subject in i:
+            print(f"{i} gefunden")
+            path = folder + i
+            shutil.move(path, target)
 
-print("Test pushen - Ergänzung von github")
+
+
+
+clean_folder("Bank", "C:/Users/mkarm/Downloads/", "C:/Users/mkarm/OneDrive/Michi/Versicherungen")
+
+
 
 
 
---
+++ dev/main.py
@@ -0,0 +1,7 @@
+#import Download_sortieren
+from Download_sortieren import clean_folder
+
+# if __name__ == '__main__':
+#    clean_folder(subject, folder, target)
+
+print("test")
