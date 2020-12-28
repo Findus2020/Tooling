c149116579aafa5bf46be92b09137b96a5f98f74
Findus2020 <m.karmanski@web.de>
2020-12-28 19:21:10 +0100

    V1.1: Sortierung anhand Dateinamen und PDF-Inhalt

--- dev/Download_sortieren.py
+++ dev/Download_sortieren.py
@@ -1,11 +1,18 @@
-# Datei verschieben
+"""Ordner durchsuchen und Dateien verschieben"""
 import os
 import shutil
+import PyPDF2
 
-def clean_folder(name, subject, folder, target):
+
+def clean_folder(name, subject, folder, target, pdf_inhalt=None):
+    """Funktion die einen Ordner durchsucht und Dateien verschieben. \n
+    name: Belieber Name der Suche \n
+    subject: Bestandteil des Dateinamens \n
+    folder: Durchsuchter Ordner \n
+    target: Zielordner \n
+    pdf_inhalt: Text-Bestandteil innerhalb des PDF"""
+
     folder_list = os.listdir(folder)
-    print(type(folder_list))
-    print(subject in folder_list)
     k = 1
     for i in folder_list:
         # print(f"{k}. Datei: {i}") #Ausgabe aller Datein im Quellordner
@@ -13,14 +20,23 @@ def clean_folder(name, subject, folder, target):
         if subject in i:
             print(f"{i} gefunden")
             path = folder + i
-            shutil.move(path, target)
-            print(
-                f"Datei: {i}, Quelle: {folder}, Ziel: {target}")
+            if pdf_inhalt == None:
+                shutil.move(path, target)
+                print(f"Datei: {i}, Quelle: {folder}, Ziel: {target}")
+            else:
+                pdfFileObj = open(path, 'rb')  # creating a pdf file object
+                pdfReader = PyPDF2.PdfFileReader(pdfFileObj)  # creating a pdf reader object
+                pageObj = pdfReader.getPage(0)  # creating a page object
+                if pdf_inhalt in pageObj.extractText():  # extracting text from page
+                    pdfFileObj.close()
+                    shutil.move(path, target)
+                    print(f"Datei: {i}, Quelle: {folder}, Ziel: {target}, PDF-Inhalt: {pdf_inhalt}")
+                else:
+                    print("Dateiname passend, jedoch den entsprechenden PDF-Inhalt nicht gefunden")
+                    pdfFileObj.close()
 
     print(f"{name}: Aufräumen beendet")
 
 
+
 #exe bauen und in autostart hinzufügen
-#Buch S. 325
-#comdirect gleich zwischen Gemeinschaftskonto und Einzelkonto
-#klarmobil gleich zwischen Mama und Papa
--- dev/main.py
+++ dev/main.py
@@ -9,3 +9,17 @@ clean_folder("Audi-Bank", "Kontoauszug-1519125726", "C:/Users/mkarm/Downloads/",
 # Renault-Bank
 clean_folder("Renault-Bank", "29131500", "C:/Users/mkarm/Downloads/",
              "C:/Users/mkarm/OneDrive/Michi/Konto/Renault Bank/Kontoauszug/2020")
+# Comdirect-Gemeinschaftskonto
+clean_folder("Comdirect", "Finanzreport", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Konto/Comdirect Gemeinschaftskonto/Finanzreport 2020", "Liza")
+# Comdirect-Einzelkonto
+clean_folder("Comdirect", "Finanzreport", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Konto/Comdirect/Finanzreport 2020", "4853123")
+
+# Klarmobil Mama
+clean_folder("Klarmobil Mama", "Rechnung", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Klarmobil/Mama/Rechnungen 2020", "1053324032")
+
+# Klarmobil Papa
+clean_folder("Klarmobil Papa", "Rechnung", "C:/Users/mkarm/Downloads/",
+             "C:/Users/mkarm/OneDrive/Michi/Klarmobil/Papa/Rechnungen 2020", "1053809118")
