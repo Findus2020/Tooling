2fa40d48a7784bc73ce2a044250093d4b30e3f6a
Findus2020 <m.karmanski@web.de>
2024-01-10 12:08:12 +0100

    Test 2024

--- dev/Download_sortieren.py
+++ dev/Download_sortieren.py
@@ -18,7 +18,7 @@ def clean_folder(name, subject, folder, target, pdf_inhalt=None):
         # print(f"{k}. Datei: {i}") #Ausgabe aller Datein im Quellordner
         k += 1
         if subject in i:
-            print(f"{i} gefunden")
+            print(f"{name}: {i} gefunden")
             path = folder + i
             if pdf_inhalt == None:
                 shutil.move(path, target)
@@ -30,13 +30,13 @@ def clean_folder(name, subject, folder, target, pdf_inhalt=None):
                 if pdf_inhalt in pageObj.extractText():  # extracting text from page
                     pdfFileObj.close()
                     shutil.move(path, target)
-                    print(f"Datei: {i}, Quelle: {folder}, Ziel: {target}, PDF-Inhalt: {pdf_inhalt}")
+                    print(f"{name}:Datei: {i}, Quelle: {folder}, Ziel: {target}, PDF-Inhalt: {pdf_inhalt}")
                 else:
-                    print("Dateiname passend, jedoch den entsprechenden PDF-Inhalt nicht gefunden")
+                    print(f"{name}: Dateiname passend, jedoch den entsprechenden PDF-Inhalt nicht gefunden")
                     pdfFileObj.close()
 
     print(f"{name}: Aufräumen beendet")
 
-
+#abfrage einbauen, ob der Pfad exisitert bzw diesen erst erstellen. Wenn der Ordner nicht exisitert, wird eine Datei erstellt die nicht geöffnet werden kann
 
 #exe bauen und in autostart hinzufügen
